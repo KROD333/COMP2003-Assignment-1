f641e024ac17b7bf29b190a1d2a355f77369b4b3
Kevin <kevinrodk@hotmail.com>
2025-01-29 10:32:49 -0500

    Add files via upload

---
+++ Group3_Assignment1.sql
@@ -0,0 +1,22 @@
+/* Assignment 1 Submission
+Date: 1/31/2025
+Group 3	    
+Student Name 		Student Number
+Kevin Rodriguez		200563852
+Roshan G C			200614949
+Tobiloba Olayoriju 	200565955
+Sumit Rokaya 		200597554
+Shota Uchikawa 		200613595
+	
+*/
+
+# **** Tips - You can remove this section when submitting ****
+# Use comments to indicate the question number that each statement addresses.
+# See the format provided for Q1 - Q2 to format the remainder of your SQL script
+# Use proper SQL Standards when formatting your script
+
+-- Q1) Replace this with a brief descriptive comment for each query. Explain the function & details of the command, don't just repeat the question.
+CREATE
+
+-- Q2) Provide a descriptive comment here.
+USE
