381085b37792b92cc7ca935ba2369ba2a13ecf2c
Kevin <kevinrodk@hotmail.com>
2025-01-29 10:36:37 -0500

    Updated Questions 1 & 2

--- Group3_Assignment1.sql
+++ Group3_Assignment1.sql
@@ -3,7 +3,7 @@ Date: 1/31/2025
 Group 3	    
 Student Name 		Student Number
 Kevin Rodriguez		200563852
-Roshan G C			200614949
+Roshan G C		200614949
 Tobiloba Olayoriju 	200565955
 Sumit Rokaya 		200597554
 Shota Uchikawa 		200613595
@@ -15,8 +15,10 @@ Shota Uchikawa 		200613595
 # See the format provided for Q1 - Q2 to format the remainder of your SQL script
 # Use proper SQL Standards when formatting your script
 
--- Q1) Replace this with a brief descriptive comment for each query. Explain the function & details of the command, don't just repeat the question.
+Kevin. R
+-- This will create the main database for the script with the name barrie_weather_db.
 CREATE DATABASE barrie_weather_db; 
 
+Kevin. R
 -- This will ensure that all queries will be executed in the created database.
 USE barrie_weather_db;
